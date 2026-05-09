670c46e826f9e97187e7b89f0078ad3842bb7577
Hrishikesh Tiwari <tiwarihrishikesh686@gmail.com>
2026-05-09 10:56:34 +0530

    tested a piece of code

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -4,13 +4,13 @@ import numpy as np
 
 # obtain an image 
 
-test_image_path = "/Users/hrishikeshtiwari/Desktop/project_root/tests/test_images/Bikesgray.jpg"
+test_image_path = "/Users/hrishikeshtiwari/Desktop/project_root/tests/test_images/hrishikesh.jpeg"
 img = pixrr.handle_image(test_image_path)
 
 
 # do some thresholding and contour extraction 
 
-img2 = pixrr.threshold_image(img,50,inverse=True)
+img2 = pixrr.otsu_thresholding(img,inverse=True)
 pixrr.contour_extractor(img2,save=True)
 
 
